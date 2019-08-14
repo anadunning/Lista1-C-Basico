14a243db2fd45ba948a01b5c32360fdb4ca8abee
anadunning <anakpedrosa@hotmail.com>
2019-08-13 22:43:46 -0300

    Read the length in inches and convert it to centimetres

---
+++ 16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+float inchesToCentim(float P);
+
+int main(){
+
+    float inches, cent;
+
+    printf("Informe o comprimento em polegadas: ");
+
+    scanf("%f", &inches);
+
+    cent = inchesToCentim(inches);
+
+    printf("O comprimento %.2f em polegadas corresponde a %.2f em centimetros.\n", inches, cent);
+
+    return 0;
+}
+
+float inchesToCentim(float P){
+
+    float C;
+
+    C = P * 2.54;
+
+    return C;
+}
